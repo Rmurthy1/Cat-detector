8a1545e35359228d6610b5645398c9bc627d651c
Rmurthy1 <rahul.a.murthy@gmail.com>
2024-02-09 01:39:06 -0800

    sending some serial data from the probe

--- catDetector.cpp
+++ catDetector.cpp
@@ -36,11 +36,15 @@ const byte txPin = 3;
 const byte trigPin = 4;
 const byte echoPin = 5;
 
+
 // Set up a new SoftwareSerial object
 SoftwareSerial mySerial (rxPin, txPin);
 
 String message = "";
 bool messageReady = false;
+
+int distanceFromSensor();
+
 
 void setup() {
   Serial.begin(9600);
@@ -50,22 +54,78 @@ void setup() {
   pinMode(txPin, OUTPUT);
 
   // pins for the HC-SR04
-  //pinMode(uint8_t pin, uint8_t mode)
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
     
   // Set the baud rate for the SoftwareSerial object
   mySerial.begin(9600);
 }
 
 void loop() {
-  while (Serial.available()) { 
+  // if there is data available from the software serial port
+  /*while (Serial.available()) { 
     message = Serial.readString();
     messageReady = true;
     Serial.println("wrote");
-  }
+  }*/
+  
+  // if there is a message to send
   if (messageReady == true) {
-    //do stuff
     messageReady = false;
     mySerial.write(message.c_str());
+    Serial.println("sent");
   }
 
+  // every 500ms, read the distance from the sensor
+  if (millis() % 5000 == 0) {
+    int distance = distanceFromSensor();
+    Serial.println(distance);
+    message = String(distance);
+    messageReady = true;
+  }
 }
+
+// function to read the distance from the ultrasonic sensor
+int distanceFromSensor() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = (duration/2) / 29.1;
+  return distance;
+}
+
+
+/*
+#define TRIG_PIN 4
+#define ECHO_PIN 5
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  long distance = (duration/2) / 29.1;
+
+  if (distance >= 400 || distance <= 2){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
+
+*/
